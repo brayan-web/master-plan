fa8fb76c36888617b15b229dfb697cae43d5008f
brayan <brayanesquivel777@gmail.com>
2022-08-17 11:04:27 -0500

    PDF IMPRIMIR

--- src/components/PDF.vue
+++ src/components/PDF.vue
@@ -2,29 +2,29 @@
 <template>
     <div>
  
-        <button class="btn btn-success" @click="printWindow()">Print</button>
-        <!-- <button class="btn btn-primary mt-5" @click="window.print()">print</button>
+        <!-- <button class="btn btn-success" @click="printWindow()">Print</button> -->
+        <button class="btn btn-primary mt-5" @click="imprimir()">IMPRIMIR PDF</button>
         <div style="width: 50%">
-            <pdf v-if="show" ref="pdf" style="border: 1px solid red" :src="src" :page="page" :rotate="rotate" @password="password" @progress="loadedRatio = $event" @error="error" @num-pages="numPages = $event" @link-clicked="page = $event"></pdf>
-        </div> -->
+            <pdf v-if="show" ref="pdf" style="border: 1px solid red" :src="src" :page="page" :rotate="rotate"   @num-pages="numPages = $event" ></pdf>
+        </div>
     </div>
 </template>
 
 <script>
-// import pdf from 'vue-pdf'
+import pdf from 'vue-pdf'
 
 export default {
     components: {
-        // pdf: pdf
+        pdf: pdf
     },
     data() {
         return {
-            // show: true,
-            // src: '/files/09a343eb-987e-45c1-a0b4-8810a81e71d6',
-            // loadedRatio: 0,
-            // page: 1,
-            // numPages: 1,
-            // rotate: 0,
+            show: true,
+            src: '/files/7eb5c325-2437-4df3-83b9-23a5de766ebb',
+            loadedRatio: 0,
+            page: 1,
+            numPages: 1,
+            rotate: 0,
         }
     },
     methods: {
@@ -36,14 +36,14 @@ export default {
 
         //     console.log(err);
         // },
-        // imprimir() {
-        //     alert("hola")
-        //     if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
-        //         window.location.reload();
-        //         setTimeout(this.$refs.pdf.print(), 1000);
+        imprimir() {
+  
+          
+            
+                setTimeout(this.$refs.pdf.print(), 1000);
 
-        //     }
-        // },
+            
+        },
         // isMobile() {
         //     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         //         return true
