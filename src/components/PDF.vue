b6e1c15509ab7ab437e1419a187ca23b9d52ba38
brayan <brayanesquivel777@gmail.com>
2022-08-17 15:39:39 -0500

    asasddfssdfsdfsdf

--- src/components/PDF.vue
+++ src/components/PDF.vue
@@ -1,75 +1,50 @@
-
 <template>
-    <div>
-    
-        <!-- <button class="btn btn-success" @click="printWindow()">Print</button> -->
-        <button class="btn btn-primary mt-5" @click="imprimir()">IMPRIMIR PDF</button>
-        <button class="btn btn-primary mt-5" @click="imprimirTest()">IMPRIMIR TEST</button>
-        <div style="width: 50%">
-            <pdf v-if="show" ref="pdf" style="border: 1px solid red" :src="src" :page="page" :rotate="rotate" @progress="loadedRatio = $event" @num-pages="numPages = $event" @link-clicked="page = $event"></pdf>
-        </div>
-    
-        <!-- <div style="width: 50%">
-            <pdf v-if="show" ref="pdfTest" style="border: 1px solid red" :src="srcTest" :page="page" :rotate="rotate" @progress="loadedRatio = $event" @num-pages="numPages = $event" @link-clicked="page = $event"></pdf>
-        </div> -->
-    </div>
+	<div>
+		<input type="checkbox" v-model="show">
+		<select v-model="src" style="width: 30em">
+			<option v-for="(item, index) in pdfList" :key="index" :value="item" v-text="item"></option>
+		</select>
+		<input v-model.number="page" type="number" style="width: 5em"> /{{numPages}}
+		<button @click="rotate += 90">&#x27F3;</button>
+		<button @click="rotate -= 90">&#x27F2;</button>
+		<button @click="$refs.pdf.print()">print</button>
+		<div style="width: 50%">
+			<div v-if="loadedRatio > 0 && loadedRatio < 1" style="background-color: green; color: white; text-align: center" :style="{ width: loadedRatio * 100 + '%' }">{{ Math.floor(loadedRatio * 100) }}%</div>
+			<pdf v-if="show" ref="pdf" style="border: 1px solid red" :src="src" :page="page" :rotate="rotate" @password="password" @progress="loadedRatio = $event" @error="error" @num-pages="numPages = $event" @link-clicked="page = $event"></pdf>
+		</div>
+	</div>
 </template>
-
 <script>
 import pdf from 'vue-pdf'
-var publicTask = pdf.createLoadingTask('/files/7eb5c325-2437-4df3-83b9-23a5de766ebb.pdf')
+
 export default {
-    components: {
-        pdf: pdf
-    },
-    data() {
-        return {
-            show: true,
-            src: publicTask,
-            srcTest: 'https://cdn.jsdelivr.net/gh/mozilla/pdf.js@c6e8ca86/test/pdfs/annotation-link-text-popup.pdf',
-            loadedRatio: 0,
-            page: 1,
-            numPages: 0,
-            rotate: 0,
-        }
-    },
-    mounted() {
-       
-        // that.src.promise.then(pdf => {
-        //   that.numPages = pdf.numPages;
-        // });
-    },
-    methods: {
-        // password: function(updatePassword) {
+	components: {
+		pdf: pdf
+	},
+	data () {
+		return {
+			show: true,
+			pdfList: [
+				'',
+				'/files/7eb5c325-2437-4df3-83b9-23a5de766ebb.pdf',
+				
+			],
+			src:'',
+			loadedRatio: 0,
+			page: 1,
+			numPages: 0,
+			rotate: 0,
+		}
+	},
+	methods: {
+		password: function(updatePassword) {
 
-        //     updatePassword(prompt('password is "test"'));
-        // },
-        // error: function(err) {
+			updatePassword(prompt('password is "test"'));
+		},
+		error: function(err) {
 
-        //     console.log(err);
-        // },
-        imprimir() {
-
-            setTimeout(this.$refs.pdf.print(), 1000);
-
-
-        },
-        imprimirTest() {
-
-            setTimeout(this.$refs.pdfTest.print(), 1000);
-
-
-        },
-        // isMobile() {
-        //     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        //         return true
-        //     } else {
-        //         return false
-        //     }
-        // },
-        printWindow: function() {
-            setTimeout(window.print(), 1000)
-        }
-    }
+			console.log(err);
+		}
+	}
 }
 </script>
