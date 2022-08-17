2677b551a484396cc4d304cfd9e42f63f3b4468a
brayan <brayanesquivel777@gmail.com>
2022-08-17 16:11:55 -0500

    aasasddfsdf

--- src/components/PDF.vue
+++ src/components/PDF.vue
@@ -1,48 +1,29 @@
 <template>
-    <div>
-        <button @click="$refs.pdf.print()">print</button>
-        <button @click="$refs.pdfE.print()">print Example</button>
-        <iframe src="/files/7eb5c325-2437-4df3-83b9-23a5de766ebb.pdf" frameborder="0"></iframe>
-        <iframe src="/files/7eb5c325-2437-4df3-83b9-23a5de766ebb.pdf" frameborder="0"></iframe>
-        <h1>File</h1>
-        <vue-pdf-embed ref="pdf" :source="src" />
-        <vue-pdf-embed ref="pdfE" :source="srcExample" />
+    <div style="background: #000; height: 100vh">
+        <button class="btn" @click="imprimir">IMPRIMIR PRINTJS</button>
+        <button class="btn" @click="imprimir">IMPRIMIR NATIVO</button>
     </div>
 </template>
 
 <script>
-// import pdf from 'vue-pdf'
-import VuePdfEmbed from 'vue-pdf-embed/dist/vue2-pdf-embed'
+// import print from 'print-js'
+import printJS from 'print-js'
 export default {
     components: {
-        // pdf: pdf
-        VuePdfEmbed,
+       
     },
     data() {
         return {
-            // show: true,
-            // pdfList: [
-            // 	'',
-            // 	'/files/7eb5c325-2437-4df3-83b9-23a5de766ebb.pdf',
-
-            // ],
-            src: '/files/7eb5c325-2437-4df3-83b9-23a5de766ebb.pdf',
-            srcExample: '/files/pdfexample.pdf',
-            // loadedRatio: 0,
-            // page: 1,
-            // numPages: 0,
-            // rotate: 0,
+            url: "https://cdn.jsdelivr.net/gh/mozilla/pdf.js@c6e8ca86/test/pdfs/annotation-link-text-popup.pdf"
         }
     },
     methods: {
-        // password: function(updatePassword) {
-
-        // 	updatePassword(prompt('password is "test"'));
-        // },
-        // error: function(err) {
-
-        // 	console.log(err);
-        // }
+      imprimir(){
+        printJS(this.url)
+      },
+       imprimirNativo(){
+        window.print(printJS(this.url))
+      }
     }
 }
 </script>
