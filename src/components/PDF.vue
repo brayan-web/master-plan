5b7771995ad47eb046ae21ad0ff268d2fefd481e
brayan <brayanesquivel777@gmail.com>
2022-08-17 23:31:39 -0500

    aasasddfsdfasdasdassdfsdfsdfsdf

--- src/components/PDF.vue
+++ src/components/PDF.vue
@@ -1,30 +1,38 @@
 <template>
     <div>
-        <button class="btn btn-primary" @click="downoaldPdf()">descarga local</button>
-      <iframe id="reportBGID" src="/files/7eb5c325-2437-4df3-83b9-23a5de766ebb.pdf" width="100%" frameborder="0"></iframe>
+        <button @click="Print" class="btn btn-primary">Imprimir</button>
+        <pdf v-if="show" ref="pdf" src="/7eb5c325-2437-4df3-83b9-23a5de766ebb.pdf" :page="page" :rotate="rotate"    @num-pages="numPages = $event" @link-clicked="page = $event"></pdf>
     </div>
 </template>
 
 <script>
-
-
+import pdf from 'vue-pdf'
+// import printJS from 'print-js'
 export default {
     components: {
-
+        pdf
     },
     data() {
         return {
-            dataUrl: 'https://cdn.rawgit.com/mozilla/pdf.js/c6e8ca86/test/pdfs/freeculture.pdf'
+            src: '',
+            show: true,
+            urlPdf: "../assets/img/core/7eb5c325-2437-4df3-83b9-23a5de766ebb.pdf",
+            page: 1,
+            numPages: 0,
+            rotate: 0,
+            url: "https://cdn.jsdelivr.net/gh/mozilla/pdf.js@c6e8ca86/test/pdfs/annotation-link-text-popup.pdf"
         }
     },
-    computed: {
+    computed:{
+       
+    },
+    mounted() {
 
     },
-    mounted() {},
     methods: {
-        downoaldPdf() {
-           document.getElementById("reportBGID").contentWindow.print()
-        }
+            Print(){
+                this.$refs.pdf.print()
+            }
     }
 }
 </script>
