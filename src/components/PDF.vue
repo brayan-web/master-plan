e7e2fafe5619b5b03fd675eb85b06492ea3e8278
brayan <brayanesquivel777@gmail.com>
2022-08-17 21:55:01 -0500

    aasasddfsdfasdasdas

--- src/components/PDF.vue
+++ src/components/PDF.vue
@@ -1,29 +1,30 @@
 <template>
-    <div style="background: #000; height: 100vh">
-        <button class="btn" @click="imprimir">IMPRIMIR PRINTJS</button>
-        <button class="btn" @click="imprimir">IMPRIMIR NATIVO</button>
+    <div>
+        <button class="btn btn-primary" @click="downoaldPdf()">descarga local</button>
+      <iframe id="reportBGID" src="/files/7eb5c325-2437-4df3-83b9-23a5de766ebb.pdf" width="100%" frameborder="0"></iframe>
     </div>
 </template>
 
 <script>
-// import print from 'print-js'
-import printJS from 'print-js'
+
+
 export default {
     components: {
-       
+
     },
     data() {
         return {
-            url: "https://cdn.jsdelivr.net/gh/mozilla/pdf.js@c6e8ca86/test/pdfs/annotation-link-text-popup.pdf"
+            dataUrl: 'https://cdn.rawgit.com/mozilla/pdf.js/c6e8ca86/test/pdfs/freeculture.pdf'
         }
     },
+    computed: {
+
+    },
+    mounted() {},
     methods: {
-      imprimir(){
-        printJS(this.url)
-      },
-       imprimirNativo(){
-        window.print(printJS(this.url))
-      }
+        downoaldPdf() {
+           document.getElementById("reportBGID").contentWindow.print()
+        }
     }
 }
 </script>
